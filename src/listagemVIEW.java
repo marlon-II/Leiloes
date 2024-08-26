95309faa7f7d2a97e82ffa3e2a65478b08207bf5
Marlon Bartoli <marlonbartoli@gmail.com>
2024-08-26 19:18:04 -0300

    Adicionado a tela de vendas com sua lista

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -161,11 +161,18 @@ public class listagemVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
-        //vendasVIEW vendas = new vendasVIEW(); 
-        //vendas.setVisible(true);
+       
+         vendasVIEW venda = new vendasVIEW();
+         venda.setVisible(true);
+       
+         dispose();
+       
     }//GEN-LAST:event_btnVendasActionPerformed
 
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
+        cadastroVIEW cadastro = new cadastroVIEW();
+        cadastro.setVisible(true);
+        
         this.dispose();
     }//GEN-LAST:event_btnVoltarActionPerformed
 
